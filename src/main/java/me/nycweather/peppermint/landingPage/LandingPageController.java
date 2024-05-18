796306ad2d510e95838e01024883948aadcdcd02
nycweather <alifftw@gmail.com>
2024-05-18 00:51:55 -0400

    deploy jar v6

--- src/main/java/me/nycweather/peppermint/landingPage/LandingPageController.java
+++ src/main/java/me/nycweather/peppermint/landingPage/LandingPageController.java
@@ -3,11 +3,13 @@ package me.nycweather.peppermint.landingPage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 @RestController
+@RequestMapping("/api/v1")
 public class LandingPageController {
 
     @GetMapping("/hello")
